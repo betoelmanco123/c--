dca453c785f81556b4db353d06cc5107dd785c6f
betoelmanco123 <robert.rom.es@icloud.com>
2025-09-04 20:14:04 -0600

    Finished

--- Practica 0/Get_matrix.cpp
+++ Practica 0/Get_matrix.cpp
@@ -1,25 +1,25 @@
-#include<iostream>
-using namespace std;
+#include<iostream>//Importa las biblilotecas
 
-int main() {
-    int m, n;
-    cout << "Numero de filas en tu matriz: ";
-    cin >> m;
-    cout << "Numero de columnas en tu matriz: ";
-    cin >> n;
-    int matriz[m][n];
-    for (int i=0; i<m; i++) {
-        for (int j=0; j<n; j++) {
-            cout << "Cual es el valor de la posición (" << i << "," << j << "): ";
-            cin >> matriz[i][j];
-        }
-    }
-    cout << "Tu matriz es: " << endl;
-    for (int i=0; i<m; i++) {
-        for (int j=0; j<n; j++) {
-            cout << matriz[i][j] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
+using namespace std;// Comando que me permite ignorar el escribir std::
+
+int main() { // Funcion principal
+	int i,j,k,m,n; //Inicializamos las variables
+	int	matrices[3][3][3]; // Inicializamos la matriz que almacenara las matrices
+	for (k=0; k<2; k++) { //Creamos un bucle que iterara sobre cada matriz
+		for (i=0; i<3; i++) { //Creamos un bucle que iterara sobre cada columna
+			for (j=0; j<3; j++) { //Creamos un bucle que iterara sobre cada fila
+				cout << "¿Cual es el valor de la matriz "<< k + 1 << " para la posicion (" << i << "," << j<< ")" ;//Pregutamos el valor en la posicion (i,j) de la matriz k +1 (para evitar tener matriz0)
+				cin >> matrices[k][i][j];//Recibimos el valor del usuario y lo asignamos a la matriz en la posicion correspondiente
+			}
+		}
 }
+	for (m=0; m<3; m++){ // Creamos un bucle que recorrera las columnas
+		for (n=0; n<3; n++){ //Creamos un bucle que iterara sobre cada fila
+			matrices[2][m][n] = matrices[0][m][n] + matrices[1][m][n]; //Sumamos los valores de las posiciones m y n y los almacenamos en la posicion (m,n) en la matriz resultante
+			cout << matrices[2][m][n] << " "; //Desplegamos el valor junto con un espacio para mostrar el resultado
+		
+			}
+			cout << endl; //Desplegamos los saltos de linea para diferenciar entre filas y columnas
+		}
+		return 0;//Terminamos el programa
+	}
