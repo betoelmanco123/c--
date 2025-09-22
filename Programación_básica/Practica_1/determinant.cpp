c36620aee5e2c12b45145fb5329f37e6af880a7a
betoelmanco123 <robert.rom.es@icloud.com>
2025-09-21 18:20:32 -0600

    advance

--- Programación_básica/Practica_1/determinant.cpp
+++ Programación_básica/Practica_1/determinant.cpp
@@ -14,43 +14,54 @@ int main() {
     };
 
     for (k=0; k<m; k++){
-        int aux[m-1][m-1];
+        int copy[m][m];
         for (i=0; i<m; i++){
             for (j=0; j<n; j++){
-                if (i==0 | j==k){
-                    continue;
-                }
-                //cout << "( "<< i << ", " << j << ")";
-                if (k==0){
-                     // works cout << "( " << i-1 << ", " << j-1 << ")";
-                     aux[i-1][j-1] = matriz_A[i][j];
-                }else if (k==m-1){
-                    // cout << "( " << i - 1 << ", " << j << ")";
-                    aux[i-1][j] = matriz_A[i][j];
-                }else if(j>k){
-                    //cout << "j es "<< j << " y k " << k;
-                    //cout << "( " << i  << ", " << j - 1 << ")";
-                    aux[i-1][j-1] = matriz_A[i][j];
-                    
-                }else if(j<k){
-                    //cout << "( "<<  i << ", " << j << ")";
-                    aux[i-1][j] = matriz_A[i][j];
-
-                }
-
-                    
-
-        }
-        }
-        cout << "value" << matriz_A[0][k] << endl;
-        cout << "---------" << endl;
-        for (i=0; i<3; i++){
-            for (j=0; j<3; j++){
-                cout << aux[i][j] << " " ;
+                copy[i][j] = matriz_A[i][j];
             }
-            cout << endl ;
+            
         }
+        // while (m>2){
+            int aux[m-1][m-1];
+            for (i=0; i<m; i++){
+                for (j=0; j<m; j++){
+                    if (i==0 | j==k){
+                        continue;
+                    }
+                    //cout << "( "<< i << ", " << j << ")";
+                    if (k==0){
+                        // Elimina el caso mas sencillo, se escogio el valor (0,0)
+                        aux[i-1][j-1] = copy[i][j];
+                    }else if (k==m-1){
+                        // Elimina el otro caso sencillo, se escogio el valor (-1,-1)
+                        aux[i-1][j] = copy[i][j];
+                    }else if(j>k){
+                        //se maneja el caso más "difícil", cuando se escoge un valor entre (0, -1), especificament cuando se el tiene que 
+                        // restar uno a a las columnas para que coincida
+                        aux[i-1][j-1] = copy[i][j];
+                        
+                    }else if(j<k){
+                        //cout << "( "<<  i << ", " << j << ")";
+                        aux[i-1][j] = copy[i][j];
+                        
+                    }
+                    
+                    
+                    
+                }
+            }
+            m = m-1;
+            cout << "value: " << matriz_A[0][k] << endl;
+            cout << "---------" << endl;
+            for (i=0; i<m; i++){
+                for (j=0; j<m; j++){
+                    cout << aux[i][j] << " " ;
+                }
+                cout << endl ;
+            }
+            m = n;
     }
+//}
 };
 
 
