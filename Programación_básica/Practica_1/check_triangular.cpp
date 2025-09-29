efc555ff1e7b339c5431c075f4f255a3701e86ae
betoelmanco123 <robert.rom.es@icloud.com>
2025-09-29 17:06:47 -0600

    pipipipi

--- Programación_básica/Practica_1/check_triangular.cpp
+++ Programación_básica/Practica_1/check_triangular.cpp
@@ -2,13 +2,15 @@
 
 using namespace std;
 bool is_running = 1;
-bool up = 1;
-bool down = 1;
+bool up = true;
+bool down = true;
 char value;
 int m;//Size
 int i,j; // Index
 
-void transpose() {
+void check_triangular() {
+    up = true;
+    down = true;
     cout <<  "How many columns and rows does the matrix have?\n";
     cin >> m;
     int matriz_A[m][m];
@@ -25,33 +27,31 @@ void transpose() {
             }
             if (i<j && up){//Above the diagonal
                 if (matriz_A[i][j] != 0){
-                    up = 0;
+                    up = false;
                 }
-            }else{//Below the diagonal
-                if (down){
+            }else if (i>j && down){//Below the diagonal
                     if (matriz_A[i][j] != 0){
-                        down = 0;
+                        down = false;
                     }
-                }
             }
                 
         }           
     }
     if (up && down){
         cout << "La matriz es triangular inferior y superior\n";
+
     }else if (up){
         cout << "La matriz es triangular superior\n";
+
     }else if(down){
         cout << "La matriz es triangular inferior\n";
-    
-    }else{
-        cout << "La matriz no es triangular\n";
-    }
-}
 
+    }else{
+    cout << "La matriz no es triangular\n";}
+}
 int main(){
     while (is_running){
-        transpose();
+        check_triangular();
         cout << "do you wanna continue? (y/n): ";
         cin >> value;
         if (value == 'n'){
